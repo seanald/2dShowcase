e041b32571011e4df1ef9b5b9e3e0391f56e0049
Sean McDonald <s.michael.mcdonald@gmail.com>
2016-01-22 18:15:46 -0500

    New project

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float speed = 1.0f;
+
+	void FixedUpdate ()
+	{
+		float v = Input.GetAxis ("Vertical");
+		float h = Input.GetAxis ("Horizontal");
+		ManageMovement (h, v);
+	}
+
+	void ManageMovement (float horizontal, float vertical)
+	{
+		Rigidbody2D rigidbody = GetComponent<Rigidbody2D> ();
+
+		if (horizontal != 0f || vertical != 0f) {
+			//moving true
+		} else {
+			//moving false
+		}
+
+		Vector2 movement = new Vector2 (horizontal, vertical);
+		this.transform.Translate (movement * speed);
+	}
+}
