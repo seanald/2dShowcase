e96434faf44cdf44cd10acc5cd7be233f5dc5302
Sean McDonald <s.michael.mcdonald@gmail.com>
2016-02-04 15:30:05 -0500

    new project

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,7 +3,13 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
+	private Animator animator;
 	public float speed = 1.0f;
+
+	void Start ()
+	{
+		this.animator = this.GetComponent<Animator> ();
+	}
 
 	void FixedUpdate ()
 	{
@@ -17,9 +23,30 @@ public class PlayerMovement : MonoBehaviour
 		Rigidbody2D rigidbody = GetComponent<Rigidbody2D> ();
 
 		if (horizontal != 0f || vertical != 0f) {
-			//moving true
+			if (horizontal > 0f) {
+				this.animator.SetBool ("Up", false);
+				this.animator.SetBool ("Down", false);
+				this.animator.SetBool ("Right", true);
+				this.animator.SetBool ("Left", false);
+			} else if (horizontal < 0f) {
+				this.animator.SetBool ("Up", false);
+				this.animator.SetBool ("Down", false);
+				this.animator.SetBool ("Right", false);
+				this.animator.SetBool ("Left", true);
+			} else if (vertical > 0f) {
+				this.animator.SetBool ("Up", true);
+				this.animator.SetBool ("Down", false);
+				this.animator.SetBool ("Right", false);
+				this.animator.SetBool ("Left", false);
+			} else if (vertical < 0f) {
+				this.animator.SetBool ("Up", false);
+				this.animator.SetBool ("Down", true);
+				this.animator.SetBool ("Right", false);
+				this.animator.SetBool ("Left", false);
+			}
+			this.animator.SetBool ("Idle", false);
 		} else {
-			//moving false
+			this.animator.SetBool ("Idle", true);
 		}
 
 		Vector2 movement = new Vector2 (horizontal, vertical);
---
+++ Assets/Scripts/Render/SpriteOrder.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteOrder : MonoBehaviour {
+
+	private float xPosition;
+	private float yPosition;
+
+	void LateUpdate()
+	{
+		this.xPosition = transform.position.x;
+		this.yPosition = transform.position.y;
+
+		transform.position = new Vector3 (this.xPosition, this.yPosition, this.yPosition);
+	}
+
+}
