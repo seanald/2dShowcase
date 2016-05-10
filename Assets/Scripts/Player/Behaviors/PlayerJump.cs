68800a075f7561999e94b1ac2fae54c0238dea91
Sean McDonald <s.michael.mcdonald@gmail.com>
2016-05-10 15:10:42 -0400

    New assets

--- Assets/Scripts/Player/Behaviors/PlayerJump.cs
+++ Assets/Scripts/Player/Behaviors/PlayerJump.cs
@@ -1,15 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerJump : MonoBehaviour {
+public class PlayerJump : MonoBehaviour
+{
+
+	public float jumpForce;
+
+	private bool isJumping;
+	private Rigidbody2D rigidbody2D;
+	private Vector2 jumpVector;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+		this.rigidbody2D = this.GetComponent<Rigidbody2D> ();
+		this.jumpVector = new Vector2 (0, this.jumpForce);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Update ()
+	{
+		if (Input.GetKey (KeyCode.Space)) {
+			this.rigidbody2D.AddForce (this.jumpVector, ForceMode2D.Impulse);
+		}
 	}
 }
