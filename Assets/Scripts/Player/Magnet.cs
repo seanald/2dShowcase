843d7e92ad174396543135fb0ac92a17a80102d2
Sean McDonald <s.michael.mcdonald@gmail.com>
2016-06-30 17:19:49 -0400

    Added magnet

--- Assets/Scripts/Player/Magnet.cs
+++ Assets/Scripts/Player/Magnet.cs
@@ -1,15 +1,63 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Magnet : MonoBehaviour {
+public class Magnet : MonoBehaviour
+{
 
 	// Update is called once per frame
-	void Update () {
-		Debug.DrawRay (this.transform.position, new Vector2(20,0));
+	void Update ()
+	{
 
-		RaycastHit2D raycastHit2D = Physics2D.Raycast (this.transform.position, Vector2.right, 20, Physics2D.AllLayers);
-		if (raycastHit2D != null && raycastHit2D.transform != null) {
-			Debug.Log(raycastHit2D.transform.name);
+		if (Input.GetKey (KeyCode.LeftShift)) {
+			this.UseMagnetAttract ();
+		}
+		if (Input.GetKey (KeyCode.RightShift)) {
+			this.UseMagnetPush ();
+		}else if (Input.GetKeyUp (KeyCode.LeftShift) || Input.GetKeyUp (KeyCode.RightShift)) {
+			Color color = new Color ();
+			ColorUtility.TryParseHtmlString ("#FFF", out color);
+
+			this.GetComponent<SpriteRenderer> ().color = color;
+		}
+	}
+
+	private void UseMagnetAttract ()
+	{
+		Color color = new Color ();
+		ColorUtility.TryParseHtmlString ("#F56666FF", out color);
+
+		this.GetComponent<SpriteRenderer> ().color = color;
+
+		RaycastHit2D[] raycastHit2D = Physics2D.RaycastAll (this.transform.position, Vector2.right, 100, Physics2D.AllLayers);
+
+		foreach (RaycastHit2D hit in raycastHit2D) {
+			if (hit.transform != null) {
+				
+				if (hit.transform.tag.Equals ("Magnet")) {
+					Debug.Log (hit.transform.name);
+					hit.transform.GetComponent<Rigidbody2D> ().AddForce (Vector2.left * 2000);
+				}
+			}
+		}
+	}
+
+	private void UseMagnetPush ()
+	{
+		Color color = new Color ();
+		ColorUtility.TryParseHtmlString ("#71B2FDFF", out color);
+
+		this.GetComponent<SpriteRenderer> ().color = color;
+
+		RaycastHit2D[] raycastHit2D = Physics2D.RaycastAll (this.transform.position, Vector2.right, 100, Physics2D.AllLayers);
+
+		foreach (RaycastHit2D hit in raycastHit2D) {
+			if (hit.transform != null) {
+
+				if (hit.transform.tag.Equals ("Magnet")) {
+					Debug.Log (hit.transform.name);
+					hit.transform.GetComponent<Rigidbody2D> ().AddForce (Vector2.right * 2000);
+				}
+			}
 		}
 	}
 }
